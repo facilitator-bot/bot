f5c969e140c5ea0126e9498b9c1d6b135994374e
Sasaki Kazuhiro <sasaki.kazuhiro@classmethod.jp>
2021-03-26 20:49:53 +0900

    編集フォームを追加

--- ui/src/Main.elm
+++ ui/src/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img, table, thead, th, tr, td)
-import Html.Attributes exposing (src)
-
+import Html exposing (Html, text, div, h1, img, table, thead, th, tr, td, form, label, input, p, button, a)
+import Html.Attributes exposing (src, value, type_, class, readonly, href)
+import Html.Events exposing (onClick)
 
 ---- MODEL ----
 
@@ -22,12 +22,12 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( {candidates = [{name = "hoge", slackUserId = "1234", lastActAt = 1234}
-    ,{name = "hoge", slackUserId = "1234", lastActAt = 1234}
-    ,{name = "hoge", slackUserId = "1234", lastActAt = 1234}
-    ,{name = "hoge", slackUserId = "1234", lastActAt = 1234}
-    ,{name = "hoge", slackUserId = "1234", lastActAt = 1234}
-    ,{name = "hoge", slackUserId = "1234", lastActAt = 1234}], selectedCandidate = Nothing}, Cmd.none )
+    ( {candidates = [{name = "ニャン助", slackUserId = "1234", lastActAt = 1234}
+    ,{name = "てんこ", slackUserId = "5678", lastActAt = 1234}
+    ,{name = "カヌレ", slackUserId = "9999", lastActAt = 1234}
+    ,{name = "あん", slackUserId = "1111", lastActAt = 1234}
+    ,{name = "つぶ", slackUserId = "333", lastActAt = 1234}
+    ,{name = "パトラ", slackUserId = "4444", lastActAt = 1234}], selectedCandidate = Nothing }, Cmd.none )
 
 
 
@@ -36,11 +36,14 @@ init =
 
 type Msg
     = NoOp
+    | SelectCandidate Candidate
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+update msg model = 
+    case msg of 
+        NoOp -> (model, Cmd.none)
+        SelectCandidate c -> ({model | selectedCandidate = Just c} , Cmd.none)
 
 
 
@@ -53,31 +56,51 @@ view model =
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
         , candidatesTable model.candidates
+        , editCandidateForm model.selectedCandidate
         ]
 
 candidatesTable: CandidateList -> Html Msg
 candidatesTable candidates = 
-    table []
+    table [class "table"]
         (List.concat [
             [
                 thead []
                     [
                         th [] [text "name"]
                         ,th [] [text "slackUserId"]
-                        ,th [] [text "lastAccAt"]
+                        ,th [] [text "lastActAt"]
                     ]
             ]
-            , List.map candidateTableRow candidates 
+            , List.map candidateTableRow candidates
         ])
 
 candidateTableRow: Candidate -> Html Msg
 candidateTableRow candidate = tr []
     [
-        td [] [text candidate.name]
+        td [] [a [href "#", onClick (SelectCandidate candidate)] [text candidate.name]]
         ,td [] [text candidate.slackUserId]
         ,td [] [text <| String.fromInt <| candidate.lastActAt]
     ]
 
+editCandidateForm: Maybe Candidate -> Html Msg
+editCandidateForm candidate = 
+    case candidate of 
+        Just c ->form [class "form"] [
+            p [] [
+                label [] [text "Name"]
+                ,input [value c.name, readonly True] []
+            ]
+            ,p [] [
+                label [] [text "lastActAt"]
+                ,input [value <| String.fromInt <| c.lastActAt, type_ "number"] [] 
+                ]
+            ,p [] [
+                label [] [text "slackUserId"]
+                ,input [value c.slackUserId, readonly True] []
+                ]
+            ,button [class "btn btn-primary"] [text "Save"]
+            ]
+        Nothing -> div [] [text "Empty"]
 
 ---- PROGRAM ----
 
