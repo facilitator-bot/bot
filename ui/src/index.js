2390f99e60aa098b53cc08e613b27b75d4a47afe
Sasaki Kazuhiro <sasaki.kazuhiro@classmethod.jp>
2021-03-29 23:25:11 +0900

    googleでの認証を実装

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,10 +1,23 @@
 import './main.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
-
-Elm.Main.init({
-  node: document.getElementById('root')
+gapi.load('client:auth2', async function () {
+  await gapi.client.init({
+    'apiKey': process.env.ELM_APP_GOOGLE_API_KEY,
+    'clientId': process.env.ELM_APP_GOOGLE_CLIENT_ID,
+    'scope': 'openid'
+  });
+  const auth = gapi.auth2.getAuthInstance();
+  const result = await auth.signIn();
+  result.getAuthResponse().access_token;
+  Elm.Main.init({
+    node: document.getElementById('root')
+  });
 });
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
